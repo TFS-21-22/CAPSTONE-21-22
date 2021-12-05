10e5d9ea0faa7fecb3435b7467b282d6d0ceef9c
GamerProg34 <shokugekikibou@gmail.com>
2021-12-05 16:20:33 -0500

    LoadingSlider
    Loads game in background while displaying progress of loading process on screen.

--- Assets/Scripts/Tranistion/ToLoadingScreen.cs
+++ Assets/Scripts/Tranistion/ToLoadingScreen.cs
@@ -6,9 +6,30 @@ using UnityEngine.UI;
 
 public class ToLoadingScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void LoadLevel(int sceneIndex)
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void Start()
+    {
+        StartCoroutine(LoadAsynchronously(2));
+
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
     }
 }
