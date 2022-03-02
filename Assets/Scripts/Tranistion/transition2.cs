f6bade10a5b38c4f86670f4b6e38f8f93cfd4e43
Mduarte97 <44001957+Mduarte97@users.noreply.github.com>
2022-03-02 03:29:34 -0500

    New color lerp working + controls

--- Assets/Scripts/Tranistion/transition2.cs
+++ Assets/Scripts/Tranistion/transition2.cs
@@ -6,143 +6,165 @@ public class transition2 : MonoBehaviour
 {
     // Blends between two materials
 
-    public float score;
-    public float scoreMultiplyer = 1;
+    //public float score;
+    //public float scoreMultiplyer = 1;
 
     
   
-    public Material material1;
-    public Material material2;
+    //public Material material1;
+    //public Material material2;
    
-    float lerpDuration = 5.0f;
-    float lerpTime = 0;
+    //float lerpDuration = 5.0f;
+    //float lerpTime = 0f;
     
-    public bool dreamState = false;
-    bool changedState = true;
-    bool objectInView = false;
+    //public bool dreamState = false;
+    //bool changedState = true;
+    //bool objectInView = false;
     
-    Renderer rend;
+    //Renderer rend;
 
     
 
-    public GameObject player;
+    //public GameObject player;
 
 
-    public ScoreSystem scoreSystem;
-    void Start()
-    {
+    //public ScoreSystem scoreSystem;
 
-        rend = GetComponent<Renderer>();
+    //public Color startColor;
+    //public Color endColor;
 
-        // At start, use the first material
-        rend.sharedMaterial = material1;
+    //void Start()
+    //{
+
+    //    rend = GetComponent<Renderer>();
+
+    //    // At start, use the first material
+    //    rend.sharedMaterial.color = startColor;
 
         
 
-    }
+    //}
 
-    void Update()
-    {
+    //void Update()
+    //{
        
-        if (scoreMultiplyer >= 1.0f && !dreamState)
-        {
-            dreamState = true;        
+    //    if (scoreMultiplyer >= 1.0f && !dreamState)
+    //    {
+    //        dreamState = true;        
 
-        }
+    //    }
 
-        if(scoreMultiplyer <= 1 && dreamState == true)
-        {
-            //lerpTime = 0;
-            //StartCoroutine(MatLerp(material2, material1));
-            dreamState = false;
-            //MaterialLerp(material2, material1);
-        } 
+    //    if(scoreMultiplyer <= 1 && dreamState == true)
+    //    {
+    //        //lerpTime = 0;
+    //        //StartCoroutine(MatLerp(material2, material1));
+    //        dreamState = false;
+    //        //MaterialLerp(material2, material1);
+    //    } 
   
-        if (Input.GetButtonDown("Fire1"))
-            score += 30;
+    //    if (Input.GetButtonDown("Fire1"))
+    //        score += 30;
         
-        if (Input.GetButtonDown("Fire2"))
-            score -= 30;
+    //    if (Input.GetButtonDown("Fire2"))
+    //        score -= 30;
 
         
-        if (dreamState)
-        {
-            if(rend.sharedMaterial)
-            {
-                if (rend.sharedMaterial.mainTexture == material1.mainTexture)
-                    changedState = false;
+    //    if (dreamState)
+    //    {
+    //        //if(rend.sharedMaterial)
+    //        //{
+    //        //    if (rend.sharedMaterial.mainTexture == material1.mainTexture)
+    //        //        changedState = false;
 
-                if (rend.sharedMaterial.mainTexture == material2.mainTexture)
-                    changedState = true;
+    //        //    if (rend.sharedMaterial.mainTexture == material2.mainTexture)
+    //        //        changedState = true;
 
-                MaterialLerp(material1, material2);
-            }
+    //        //  //  MaterialLerp(material1, material2);
+    //        //}
+
+    //        ColorLerp(startColor, endColor);
         
-        }
-        else
-        {
-            if(rend.sharedMaterial && material2)
-            {
-                if (rend.sharedMaterial.mainTexture == material2.mainTexture)
-                    changedState = false;
+    //    }
+    //    else
+    //    {
+    //        //if(rend.sharedMaterial && material2)
+    //        //{
+    //        //    if (rend.sharedMaterial.mainTexture == material2.mainTexture)
+    //        //        changedState = false;
 
-                if (rend.sharedMaterial.mainTexture == material1.mainTexture)
-                    changedState = true;
+    //        //    if (rend.sharedMaterial.mainTexture == material1.mainTexture)
+    //        //        changedState = true;
 
-                MaterialLerp(material2, material1);
-            }
-       
-        }
+    //        //   // MaterialLerp(material2, material1);
+    //        //}
+    //        ColorLerp(endColor, startColor);
+    //    }
         
-        if(scoreSystem)
-        {
-            score = scoreSystem.score;
-            scoreMultiplyer = scoreSystem.scoreMulitplyer;
-        }
+    //    if(scoreSystem)
+    //    {
+    //        score = scoreSystem.score;
+    //        scoreMultiplyer = scoreSystem.scoreMulitplyer;
+    //    }
         
         
-        if(player)
-        {
-            if ((player.transform.position.z + 10.0f) >= gameObject.transform.position.z)
-            {
-                objectInView = true;
-            }
-            else
-            {
-                objectInView = false;
-            }
+    //    if(player)
+    //    {
+    //        if ((player.transform.position.z + 10.0f) >= gameObject.transform.position.z)
+    //        {
+    //            objectInView = true;
+    //        }
+    //        else
+    //        {
+    //            objectInView = false;
+    //        }
 
-            if (!objectInView && !gameObject.CompareTag("DNDP"))
-            {
-                transform.parent.gameObject.SetActive(false);
-            }
+    //        if (!objectInView && !gameObject.CompareTag("DNDP"))
+    //        {
+    //            transform.parent.gameObject.SetActive(false);
+    //        }
 
-            if (!objectInView && gameObject.CompareTag("Log"))
-            {
-                gameObject.SetActive(false);
-            }
-        }
+    //        if (!objectInView && gameObject.CompareTag("Log"))
+    //        {
+    //            gameObject.SetActive(false);
+    //        }
+    //    }
       
      
-    }
+    //}
     
-    public void MaterialLerp(Material _mat1, Material _mat2)
-    { 
-        if (!changedState)
-        {
-            lerpTime += Time.deltaTime;
-           // float lerp = lerpTime / lerpDuration;
-            rend.sharedMaterial.Lerp(_mat1, _mat2, lerpTime / lerpDuration);
-        }
+    //public void MaterialLerp(Material _mat1, Material _mat2)
+    //{ 
+    //    if (!changedState)
+    //    {
+    //        lerpTime += Time.deltaTime;
+    //       // float lerp = lerpTime / lerpDuration;
+    //        rend.sharedMaterial.Lerp(_mat1, _mat2, lerpTime / lerpDuration);
+    //    }
 
-        if (lerpTime > lerpDuration)
-        {
-            lerpTime = 0;
-            changedState = true;
-            rend.sharedMaterial = _mat2;
-        }
+    //    if (lerpTime > lerpDuration)
+    //    {
+    //        lerpTime = 0;
+    //        changedState = true;
+    //        rend.sharedMaterial = _mat2;
+    //    }
 
-    }
+    //}
+
+    //public void ColorLerp(Color _startColor, Color _endColor)
+    //{
+    //    if(!changedState)
+    //    {
+    //        lerpTime += Time.deltaTime;
+
+    //        GetComponent<Renderer>().material.color = Color.Lerp(_startColor, _endColor, lerpTime / lerpDuration);
+    //    }
+
+    //    if(lerpTime > lerpDuration)
+    //    {
+    //        lerpTime = 0;
+    //        changedState = true;
+    //    }
+    //}
     
   
 
