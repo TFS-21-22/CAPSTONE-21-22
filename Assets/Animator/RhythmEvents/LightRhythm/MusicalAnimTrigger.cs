6359a30695f6d1b8bffb6f7262d95423d2d36b93
Nabila Roshd <nabila.roshd@torontofilmschool.ca>
2022-03-08 03:38:16 -0500

    Lighting and Birds

---
+++ Assets/Animator/RhythmEvents/LightRhythm/MusicalAnimTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class MusicalAnimTrigger : MonoBehaviour
+{
+    public Animation animCom;
+    [EventID]
+    public string eventID;
+
+    void Awake()
+    {
+        Koreographer.Instance.RegisterForEvents(eventID, OnAnimationTrigger);
+    }
+
+    void OnAnimationTrigger(KoreographyEvent evt)
+    {
+        animCom.Stop();
+        animCom.Play();
+    }
+}
